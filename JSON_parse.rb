6feb309191f6ebf1b6dc3c34b4bf2d2233664955
Andrey Solomin <solomin.andrey@gmail.com>
2015-05-22 23:09:32 +0300

    Added check for data type.

--- JSON_parse.rb
+++ JSON_parse.rb
@@ -4,5 +4,11 @@ require "json"
 uri = URI("https://api.github.com/users/solandmedotru")
 
 result = JSON.parse(Net::HTTP.get(uri), symbolize_names: true)
-puts result[:login]
-puts result[:location]
+
+if result.is_a?(Array)
+  result.each { |hsh| puts hsh[:login] }
+elsif result.is_a?(Hash)
+  puts result[:login]
+else
+  puts "Неверные входные данные. Необходим массив хэшей или хэш."
+end
